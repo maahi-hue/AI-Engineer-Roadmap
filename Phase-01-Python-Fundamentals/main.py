8fa298ab3d839358f7992850ebeaae5a4b6a5728
nafisakamal <nafisakamal180@gmail.com>
2026-06-05 22:34:14 +0600

    if statements done

--- Phase-01-Python-Fundamentals/main.py
+++ Phase-01-Python-Fundamentals/main.py
@@ -74,8 +74,31 @@ coordinates = [(4, 5), (6, 7), (80, 34)]
 # say_hi("mike", 20, "Male")
 
 # return statement
-def cube(num):
-    return num*num*num
+# def cube(num):
+#     return num*num*num
+#
+# result = cube(3)
+# print(result)
 
-result = cube(3)
-print(result)
+# if statement
+is_male = False
+is_tall = True
+
+# if is_male and is_tall:
+#     print("You are a tall male")
+# elif is_male and not(is_tall):
+#     print("You are a short male")
+# elif not(is_male) and is_tall:
+#     print("You are not a male but are tall")
+# else:
+#     print("you are not a male and not tall")
+
+# if statements & comparisons
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+print(max_num(3,4,5))
