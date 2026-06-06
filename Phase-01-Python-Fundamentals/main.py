303af66e0524f401dc0c780e208f479ba0b98113
nafisakamal <nafisakamal180@gmail.com>
2026-06-06 19:45:54 +0600

    exponent function 2d lists & nested loops done

--- Phase-01-Python-Fundamentals/main.py
+++ Phase-01-Python-Fundamentals/main.py
@@ -134,14 +134,39 @@ friends = ["kevin", "karen", "jim", "Oscar", "Toby"]
 # for friend in  friends:
     # print(friend)
 
-for index in range(len(friends)):
-    print(friends[index])
+# for index in range(len(friends)):
+#     print(friends[index])
+#
+# for index in range(3, 10):
+#     print(index)
+#
+# for index in range(5):
+#     if index == 0:
+#         print("First iteration")
+#     else:
+#         print("not first iteration")
 
-for index in range(3, 10):
-    print(index)
 
-for index in range(5):
-    if index == 0:
-        print("First iteration")
-    else:
-        print("not first iteration")
+# exponent function
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result *= base_num
+    return result
+
+# print(raise_to_power(3,4))
+
+# 2D lists & nested loops
+number_grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+# print(number_grid[0][0])
+
+# for row in number_grid:
+#     for col in row:
+        # print(col)
+
